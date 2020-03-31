36f85eb84a38e54105e2779a6010d7a621dcf7d7
Alessio <alessioferriweb@gmail.com>
2020-03-31 19:42:53 +0100

    Adding context menu to tray icon

--- status-tray-app/app/TimerTray.js
+++ status-tray-app/app/TimerTray.js
@@ -1,6 +1,7 @@
 const electron = require("electron");
-const {Tray} = electron;
-class TimerTray extends Tray{
+const {Tray, Menu, app} = electron;
+
+class TimerTray extends Tray {
   constructor(iconPath, mainWindow) {
     super(iconPath);
 
@@ -8,22 +9,35 @@ class TimerTray extends Tray{
 
     this.setToolTip("Timer App");
     this.on("click", this.onClick.bind(this));
+    this.on("right-click", this.onRightClick.bind(this));
   }
-  onClick(event, bounds){
+
+  onClick(event, bounds) {
     const {x, y} = bounds;
     const {height, width} = this.mainWindow.getBounds();
-    if (!this.mainWindow.isVisible()){
+    if (!this.mainWindow.isVisible()) {
       const yPosition = process.platform === "darwin" ? y : y - height;
       this.mainWindow.setBounds({
-        x: x - width/2,
-        y: y,
+        x: x - width / 2,
+        y: yPosition,
         height: height,
         width: width
       });
       this.mainWindow.show();
-    }
-    else
+    } else
       this.mainWindow.hide();
+  }
+
+  onRightClick(event, bounds) {
+    const menuConfig = Menu.buildFromTemplate([
+      {
+        "label": "Quit",
+        click() {
+          app.quit()
+        }
+      }
+    ]);
+    this.popUpContextMenu(menuConfig);
   }
 
 }
