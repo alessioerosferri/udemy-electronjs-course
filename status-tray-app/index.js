857f05a4907d700ce37f778f4ab0f8fc08025d6e
Alessio <alessioferriweb@gmail.com>
2020-03-31 20:14:23 +0100

    Adding ipc to display the time left for task completion

--- status-tray-app/index.js
+++ status-tray-app/index.js
@@ -1,6 +1,6 @@
 const electron = require("electron");
 const path = require("path");
-const {app} = electron;
+const {app, ipcMain} = electron;
 const TimeTray = require("./app/TimerTray");
 const MainWindow = require("./app/MainWindow");
 
@@ -21,4 +21,8 @@ app.on("ready", () => {
   const iconName = process.platform === "darwin" ? "iconTemplate.png" : "windows-icon@2x.png";
 
   tray = new TimeTray(path.join(__dirname, "src", "assets", iconName), mainWindow);
+});
+
+ipcMain.on("update-timer", (event, timerLeft)=>{
+  tray.setTitle(timerLeft);
 });
