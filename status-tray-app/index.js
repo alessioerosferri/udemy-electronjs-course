8788f9e99e8dcc1ab8594199a236cd91097542bb
Alessio <alessioferriweb@gmail.com>
2020-03-31 18:24:25 +0100

    Positioning the window on tray icon using Electron Bounds

--- status-tray-app/index.js
+++ status-tray-app/index.js
@@ -18,9 +18,19 @@ app.on("ready", () => {
 
   const iconName = process.platform === "darwin" ? "iconTemplate.png" : "windows-icon@2x.png";
   tray = new Tray(path.join(__dirname, "src", "assets", iconName));
-  tray.on("click", ()=>{
-    if (!mainWindow.isVisible())
+  tray.on("click", (event, bounds)=>{
+    const {x, y} = bounds;
+    const {height, width} = mainWindow.getBounds();
+    if (!mainWindow.isVisible()){
+      const yPosition = process.platform === "darwin" ? y : y - height;
+      mainWindow.setBounds({
+        x: x - width/2,
+        y: y,
+        height: height,
+        width: width
+      });
       mainWindow.show();
+    }
     else
       mainWindow.hide();
 
